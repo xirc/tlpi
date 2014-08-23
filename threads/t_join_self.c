8acdd26a49b94ca1aecbd7d28de302f52e898ef5
xirc <xirckey@gmail.com>
2014-08-23 22:42:59 +0900

    exercise 29-1

---
+++ threads/t_join_self.c
@@ -0,0 +1,38 @@
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Affero General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the file COPYING.agpl-v3 for details.                               *
+\*************************************************************************/
+
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+
+int
+main(int argc __attribute__((unused)),
+     char *argv[] __attribute__((unused)))
+{
+    int s;
+
+    printf("This process(thread) joins itself.\n");
+    printf("This process never ends?\n");
+
+    s = pthread_join(pthread_self(), NULL);
+    if (s != 0) {
+        fprintf(stderr, "pthread_join (errno=%d, errmsg=%s)\n",
+                s, strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
