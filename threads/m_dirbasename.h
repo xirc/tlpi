3e032de0f21a0b6f4c0f44803cab48a74d6ba1c0
xirc <xirckey@gmail.com>
2014-09-07 15:47:15 +0900

    exercise 31-2

---
+++ threads/m_dirbasename.h
@@ -0,0 +1,22 @@
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Affero General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the file COPYING.agpl-v3 for details.                               *
+\*************************************************************************/
+
+
+#ifndef DIRBASENAME_H
+#define DIRBASENAME_H
+
+
+char * m_basename(char const *path);
+
+
+char * m_dirname(char const *path);
+
+
+#endif
