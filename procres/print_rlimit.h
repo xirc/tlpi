8e73e4af18fa52229f847ce4906b25d11e4942b2
xirc <xirckey@gmail.com>
2014-09-17 21:07:00 +0900

    procres/rlimit_nproc

---
+++ procres/print_rlimit.h
@@ -0,0 +1,29 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2014.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Lesser General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+\*************************************************************************/
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+* See above.                                                              *
+\*************************************************************************/
+
+
+/* print_rlimit.h
+
+   Header file for print_rlimit.c.
+*/
+
+
+#ifndef PRINT_RLIMIT_H
+#define PRINT_RLIMIT_H
+
+
+int print_rlimit(const char *msg, int resource);
+
+
+#endif
