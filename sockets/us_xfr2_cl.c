5ea7234941be7b9df77809f7eed35939f1a3b6cf
xirc <xirckey@gmail.com>
2014-11-30 20:21:06 +0900

    exercise 57-2

---
+++ sockets/us_xfr2_cl.c
@@ -0,0 +1,66 @@
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Affero General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the file COPYING.agpl-v3 for details.                               *
+\*************************************************************************/
+
+
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "us_xfr2.h"
+
+
+int
+main(int argc __attribute__((unused)),
+     char *argv[] __attribute__((unused)))
+{
+    struct sockaddr_un addr;
+    int sfd;
+    ssize_t num_read;
+    char buf[BUF_SIZE];
+
+    /* Create client socket */
+    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Construct server address, and make the connection */
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(&addr.sun_path[1], SV_SOCK_PATH, sizeof(addr.sun_path) - 2);
+
+    if (connect(sfd, (struct sockaddr *) &addr,
+                sizeof(struct sockaddr_un)) == -1)
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Copy stdin to socket */
+    while ((num_read = read(STDIN_FILENO, buf, BUF_SIZE)) > 0) {
+        if (write(sfd, buf, num_read) != num_read) {
+            fprintf(stderr, "partial/failed write\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (num_read == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Closes our socket; server sees EOF */
+    exit(EXIT_SUCCESS);
+}
---
+++ sockets/us_xfr2_sv.c
@@ -0,0 +1,90 @@
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Affero General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the file COPYING.agpl-v3 for details.                               *
+\*************************************************************************/
+
+
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "us_xfr2.h"
+
+
+#define BACKLOG 5
+
+
+int
+main(int argc __attribute__((unused)),
+     char *argv[] __attribute__((unused)))
+{
+    struct sockaddr_un addr;
+    int sfd, cfd;
+    ssize_t num_read;
+    char buf[BUF_SIZE];
+
+    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Construct server socket address, bind socket to it,
+     * and make this a listening socket */
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(&addr.sun_path[1], SV_SOCK_PATH, sizeof(addr.sun_path) - 2);
+
+    if (bind(sfd, (struct sockaddr *) &addr,
+                sizeof(struct sockaddr_un)) == -1)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Handle client connections iteratively */
+    while (1) {
+        /* Accept a connection. The connection is returned on a new
+         * socket, 'cfd'; the listening socket ('sfd') remains open and
+         * can be used to accept further connections.
+         */
+        cfd = accept(sfd, NULL, NULL);
+        if (cfd == -1) {
+            perror("accpet");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Transfer data from connected socket to stdout until EOF */
+        while ((num_read = read(cfd, buf, BUF_SIZE)) > 0) {
+            if (write(STDOUT_FILENO, buf, num_read) != num_read) {
+                fprintf(stderr, "partial/failed write\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (num_read == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        if (close(cfd) == -1) {
+            perror("close");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
