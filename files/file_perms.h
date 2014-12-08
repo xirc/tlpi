e5fff43bb1257313e63e817ca0154e6fde72339c
xirc <xirckey@gmail.com>
2014-12-08 16:38:09 +0900

    files/t_stat

---
+++ files/file_perms.h
@@ -0,0 +1,36 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2014.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Lesser General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+\*************************************************************************/
+/*************************************************************************\
+*                  Copyright (C) xirc, 2014.                              *
+* See above.                                                              *
+\*************************************************************************/
+
+
+/* file_perms.h
+
+   Header file for file_perms.c.
+*/
+
+
+#ifndef FILE_PERMS_H
+#define FILE_PERMS_H
+
+
+#include <sys/types.h>
+
+
+#define FP_SPECIAL 1       /* Include set-user-ID, set-group-ID, and sticky
+                              bit information in returned string */
+
+
+char* file_perm_str(mode_t perm, int flags);
+
+
+#endif
